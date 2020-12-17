467dffa7bf95a99447a18c0c87f987e8a2ac4714
sergi grau <sergi.grau@fje.edu>
2020-12-17 20:40:57 +0100

    Add files via upload

--- m04-PassemArgumentsClient.js
+++ m04-PassemArgumentsClient.js
@@ -1,19 +1,23 @@
-var tirades = 3;
-var cares = 6;
-var consulta = `query tirarDau($tirades: Int!, $cares: Int) {
-  tirarDau(numVegades: $tirades, numCares: $cares)
-}`;
- 
-fetch('/graphql', {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/json',
-    'Accept': 'application/json',
-  },
-  body: JSON.stringify({
-    query: consulta,
-    variables: { tirades, cares },
-  })
-})
-  .then(r => r.json())
-  .then(dades => console.log('dades retornades:', dades));
+window.addEventListener('load', function () {
+  function cridarGraph() {
+    let tirades = document.getElementById('vegades').value * 1;
+    let cares = document.getElementById('cares').value * 1;
+    var consulta = `query tirarDau($tirades: Int!, $cares: Int) {
+    tirarDau(numVegades: $tirades, numCares: $cares)
+  }`;
+    fetch('/graphql', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: JSON.stringify({
+        query: consulta,
+        variables: { tirades, cares },
+      })
+    })
+      .then(r => r.json())
+      .then(dades => document.getElementById('sortida').innerText= JSON.stringify(dades));
+  }
+  document.getElementById('enviar').addEventListener('click', cridarGraph);
+});
